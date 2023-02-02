0b0d53f3818081b7b37c3c2007f6aa7f1f4beaf2
papon01 <papon0102@gmail.com>
2023-02-02 13:10:15 +0700

    loginout fix

--- public/Google_login.js
+++ public/Google_login.js
@@ -1,86 +1,133 @@
 var YOUR_CLIENT_ID = '837943993291-ockna05iiitrqkjjldcun1h623pheo2n.apps.googleusercontent.com';
-var YOUR_REDIRECT_URI = 'https://bignrecycle.web.app';
-var fragmentString = location.hash.substring(1);
+  var YOUR_REDIRECT_URI = 'https://bignrecycle.web.app';
+  var fragmentString = location.hash.substring(1);
 
-// Parse query string to see if page request is coming from OAuth 2.0 server.
-var params = {};
-var regex = /([^&=]+)=([^&]*)/g, m;
-while (m = regex.exec(fragmentString)) {
-params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
-}
-if (Object.keys(params).length > 0) {
-localStorage.setItem('oauth2-test-params', JSON.stringify(params) );
-if (params['state'] && params['state'] == 'try_sample_request') {
-    trySampleRequest();
-}
-}
-
-// If there's an access token, try an API request.
-// Otherwise, start OAuth 2.0 flow.
-function trySampleRequest() {
-var params = JSON.parse(localStorage.getItem('oauth2-test-params'));
-if (params && params['access_token']) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET',
-        'https://www.googleapis.com/drive/v3/about?fields=user&' +
-        'access_token=' + params['access_token']);
-    xhr.onreadystatechange = function (e) {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log(xhr.response);
-    } else if (xhr.readyState === 4 && xhr.status === 401) {
-        // Token invalid, so prompt for user permission.
-        oauth2SignIn();
+  // Parse query string to see if page request is coming from OAuth 2.0 server.
+  var params = {};
+  var regex = /([^&=]+)=([^&]*)/g, m;
+  while (m = regex.exec(fragmentString)) {
+    params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+  }
+  if (Object.keys(params).length > 0) {
+    localStorage.setItem('oauth2-test-params', JSON.stringify(params) );
+    if (params['state'] && params['state'] == 'singIn_state') {
+      singIn();
     }
-    };
-    xhr.send(null);
-} else {
-    oauth2SignIn();
-}
-}
+  }
 
-/*
-* Create form to request access token from Google's OAuth 2.0 server.
-*/
-function oauth2SignIn() {
-// Google's OAuth 2.0 endpoint for requesting an access token
-var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+  // If there's an access token, try an API request.
+  // Otherwise, start OAuth 2.0 flow.
+  function singIn() {
+    var params = JSON.parse(localStorage.getItem('oauth2-test-params'));
+    if (params && params['access_token']) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET',
+          'https://www.googleapis.com/drive/v3/about?fields=user&' +
+          'access_token=' + params['access_token']);
+      xhr.onreadystatechange = function (e) {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          console.log(xhr.response);
+        } else if (xhr.readyState === 4 && xhr.status === 401) {
+          // Token invalid, so prompt for user permission.
+          oauth2SignIn().then(SignOut());
+        }
+      };
+      xhr.send(null);
+    } else {
+        oauth2SignIn().then(SignOut());
+    }
 
-// Create element to open OAuth 2.0 endpoint in new window.
-var form = document.createElement('form');
-form.setAttribute('method', 'GET'); // Send as a GET request.
-form.setAttribute('action', oauth2Endpoint);
+          //singout
+        function SignOut() {
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function () {
+                console.log('User signed out.');
+                document.getElementById(singIn()).innerHTML = 'LOGIN';
+                document.getElementById(singIn()).onclick = oauth2SignIn();
+    });
+    }
 
-// Parameters to pass to OAuth 2.0 endpoint.
-var params = {'client_id': YOUR_CLIENT_ID,
-                'redirect_uri': YOUR_REDIRECT_URI,
-                'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
-                'state': 'try_sample_request',
-                'include_granted_scopes': 'true',
-                'response_type': 'token'};
+    
+  }
 
-// Add form parameters as hidden input values.
-for (var p in params) {
-    var input = document.createElement('input');
-    input.setAttribute('type', 'hidden');
-    input.setAttribute('name', p);
-    input.setAttribute('value', params[p]);
-    form.appendChild(input);
-}
+  /*
+   * Create form to request access token from Google's OAuth 2.0 server.
+   */
+  function oauth2SignIn() {
+    // Google's OAuth 2.0 endpoint for requesting an access token
+    var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
 
-// Add form to page and submit it to open the OAuth 2.0 endpoint.
-document.body.appendChild(form);
-form.submit();
-}
+    // Create element to open OAuth 2.0 endpoint in new window.
+    var form = document.createElement('form');
+    form.setAttribute('method', 'GET'); // Send as a GET request.
+    form.setAttribute('action', oauth2Endpoint);
+
+    // Parameters to pass to OAuth 2.0 endpoint.
+    var params = {'client_id': YOUR_CLIENT_ID,
+                  'redirect_uri': YOUR_REDIRECT_URI,
+                  'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+                  'state': 'singIn_state',
+                  'include_granted_scopes': 'true',
+                  'response_type': 'token'};
+
+    // Add form parameters as hidden input values.
+    for (var p in params) {
+      var input = document.createElement('input');
+      input.setAttribute('type', 'hidden');
+      input.setAttribute('name', p);
+      input.setAttribute('value', params[p]);
+      form.appendChild(input);
+    }
+
+    // Add form to page and submit it to open the OAuth 2.0 endpoint.
+    document.body.appendChild(form);
+    form.submit();
+
+
+
+       
+  }
+
+
+
+
+
+
 
 
 
 
 //on sign in
-
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
+    const profile = googleUser.getBasicProfile();
+    const userId = profile.getId();
+    const userName = profile.getName();
+    const userImage = profile.getImageUrl();
+    const userEmail = profile.getEmail();
+
+    // Do not send userId to your backend! Use an ID token instead.
+    console.log(`ID: ${userId}`);
+    console.log(`Name: ${userName}`);
+    console.log(`Image URL: ${userImage}`);
+    console.log(`Email: ${userEmail}`);
+
+
+    //porfilepic "window.userImage"
+    window.userImage = userImage;
+
+    const userImageElement = document.createElement('img');
+    userImageElement.src = window.userImage;
+    userImageElement.alt = 'User Profile Image';
+    userImageElement.style.width = '42px';
+    userImageElement.style.height = '42px';
+    userImageElement.style.borderRadius = '50%';
+
+    document.body.appendChild(userImageElement);
+
+
+    
+  
+}
+
+   
+
